4130e67063c6dc993fcb02530a59ce75a5950c2e
stkr22 <checkout@github.com>
2024-04-07 10:59:09 +0000

    fixing missing librosa dependency;

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 apt-get install -y libportaudio2 # needed by sounddevices
-apt-get install -y libspeexdsp-dev # needed for noise suppression 
+apt-get install -y libspeexdsp-dev # needed for noise suppression
+apt-get install libsndfile1 # needed for librosa - seem to be a sufficient substitute for this use case instead of using ffmpeg
